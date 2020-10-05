9a2affe342b7cb5d81048e69b12042409b2b85f8
CMD <gautamkailashsingh@gmail.com>
2020-10-05 14:08:48 +0530

    Declared board.

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -7,6 +7,8 @@ namespace TicTacToeGame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Tic Tac Toe");
+            char[] board = new char[10];
         }
+
     }
 }
