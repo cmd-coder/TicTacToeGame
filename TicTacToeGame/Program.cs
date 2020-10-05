16a96005ab682a9a7a305cd796d71c7c5a6f792b
CMD <gautamkailashsingh@gmail.com>
2020-10-05 15:12:30 +0530

    Added method to select symbol.

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -5,10 +5,12 @@ namespace TicTacToeGame
     class Program
     {
         static char[] board = new char[10];
+        static string choice = "";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Tic Tac Toe");
-            
+            UC1_initialize();
+            UC2_symbol();
             
         }
 
@@ -16,6 +18,18 @@ namespace TicTacToeGame
         {
             for (int i = 1; i < 10; i++)
                 board[i] = ' ';
+        }
+
+        static void UC2_symbol()
+        {
+            Console.WriteLine("Enter to choose O (capital o) or to choose X (capital x)");
+            choice = Console.ReadLine();
+            Console.WriteLine("You will play with: " + choice);
+            if (choice == "O")
+                Console.WriteLine("Computer will play with: X");
+            else
+                Console.WriteLine("Computer will play with: O");
+
         }
 
     }
